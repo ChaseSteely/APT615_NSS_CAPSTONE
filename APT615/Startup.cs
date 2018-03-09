c78f487c48c2e3266c13e672c65247dcb4e5387d
Chase Steely <chasesteely@gmail.com>
2018-03-08 23:01:38 -0600

    trying to get api key deployed to azure

--- APT615/Startup.cs
+++ APT615/Startup.cs
@@ -26,17 +26,20 @@ namespace APT615
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
+
             // Use SQL Database if in Azure, otherwise, use SQLite
             if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
                 services.AddDbContext<ApplicationDbContext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")))
-                        .AddSingleton<IApplicationConfiguration, ApplicationConfiguration>(
-        e => Configuration.GetSection("GoogleApiKey")
-                .Get<ApplicationConfiguration>());
+                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             else
                 services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseSqlite("Data Source=apt615.db"));
+
+            Environment.GetEnvironmentVariable("GOOGLE_MAP");
+                 services.AddSingleton<IApplicationConfiguration, ApplicationConfiguration>(
+        e => Configuration.GetSection("GoogleApiKey")
+                .Get<ApplicationConfiguration>());
+
 
             // Automatically perform database migration
             services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
