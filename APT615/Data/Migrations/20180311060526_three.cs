525e646dcbd9fa145b106d82c2c51c031fc69886
Chase Steely <chasesteely@gmail.com>
2018-03-11 00:08:20 -0600

    changed register model

---
+++ APT615/Data/Migrations/20180311060526_three.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace APT615.Data.Migrations
+{
+    public partial class three : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
