611ce910edd38b69ba221db463d6abfe98f97377
Chase Steely <chasesteely@gmail.com>
2018-03-17 15:03:50 -0500

    changed track.cshtml to use a ViewModel instead of default Entity framework Model in order to bring in Amenities and join table. Changed default Create View to Track

---
+++ APT615/Models/ApartmentViewModels/TrackViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APT615.Models.ApartmentViewModels.Track
+{
+    public class TrackViewModel
+    {
+        public Apartment Apartment { get; set; }
+        public HashSet<Amenity> Amenities { get; set; }
+
+        public TrackViewModel()
+        {
+            Amenities = new HashSet<Amenity>();
+        }
+    }
+}
